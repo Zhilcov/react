1eac1ff4be1f47e19860dc1e7bc4e40f513f9740
petya <fexsy12@gmail.com>
2019-04-08 12:13:08 +0300

    changed confirmation deletion

--- src/components/FigureItem/index.js
+++ src/components/FigureItem/index.js
@@ -1,22 +1,41 @@
 import React from 'react'
-
+import {Button, Modal} from "react-bootstrap"
 
 class FigureItem extends React.Component{
+    constructor(props, context) {
+        super(props, context);
+    
+        this.state = {
+          smShow: false
+        };
+      }
     render() {
-        
+        let smClose = () => this.setState({ smShow: false });
         const { figure , deleteFigure } = this.props;     
-      
+        
         return (
             <tr>
                 <th scope="row">{figure.id}</th>
                 <td>{figure.value}</td>
                 <td>{figure.label}</td>
-                <td className = "deletttt" ><i className="fas fa-trash-alt"  onClick={() => {
-                   var a =  window.confirm("Вы уверены что хотите удалить?");
-                    if(a){
-                        deleteFigure(figure.id)
-                    }
-                    }}></i></td>  
+                <td><i className="fas fa-trash-alt"  onClick={() => {
+                    this.setState({ smShow: true })
+                    }}></i></td>
+                    <Modal
+                        size="sm"
+                        show={this.state.smShow}
+                        onHide={smClose}
+                        aria-labelledby="example-modal-sizes-title-sm"
+                    >
+                        <Modal.Header closeButton>
+                        <Modal.Title id="example-modal-sizes-title-sm">
+                            Удалить?
+                        </Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Button className =  "btn btn-danger" onClick={()=>{deleteFigure(figure.id)}}>Удалить</Button>
+                        </Modal.Body>
+                    </Modal>  
             </tr>
         );
     }
---
+++ src/components/deletemodal/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import {Button, Modal} from "react-bootstrap"
+
+class Modal extends React.Component {
+
+
+
+    render(){
+        return(
+            <div className="d">
+            </div>
+        )
+    }
+}
